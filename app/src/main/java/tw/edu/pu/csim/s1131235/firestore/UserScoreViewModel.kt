596973c6911ceda566849691b008dd3da2c0dbda
b0966031908-jpg <b0966031908@gmail.com>
2025-11-24 16:14:43 +0800

    V3新增/修改/刪除(新增文件ID)

--- app/src/main/java/tw/edu/pu/csim/s1131235/firestore/UserScoreViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131235/firestore/UserScoreViewModel.kt
@@ -20,5 +20,19 @@ class UserScoreViewModel : ViewModel() {
         }
     }
 
+    fun updateUser(userScore: UserScoreModel) {
+        // 在 viewModelScope 中啟動一個協程
+        viewModelScope.launch {
+            // 呼叫 suspend function，並等待結果
+            message = userScoreRepository.updateUser(userScore)
+        }
+    }
 
+    fun deleteUser(userScore: UserScoreModel) {
+        // 在 viewModelScope 中啟動一個協程
+        viewModelScope.launch {
+            // 呼叫 suspend function，並等待結果
+            message = userScoreRepository.updateUser(userScore)
+        }
+    }
 }
